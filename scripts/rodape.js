e0574591790d20dc3f0c0b0d8c3470e47c476325
Joao Victor Ventura <joao.victor.ventura25@gmail.com>
2024-10-13 11:07:23 -0300

    Refatorando e adicionando herança do footer

---
+++ scripts/rodape.js
@@ -0,0 +1,13 @@
+// Arquivo responsavel por exibir / chamar o arquivo navbar
+// Codigo responsavel pela herança da barra de navegação 
+async function loadNavbar() { // Função para lidar com o carregamento de arquivos
+    try {
+        const response = await fetch('base/rodape.html'); // Busca o caminho do arquivo e armazena na const 
+        const navbar = await response.text(); // Carrega o arquivo e converte para texto, para que seja manipulado
+        document.getElementById('rodape').innerHTML = navbar; // Inserindo o conteudo armazenado na const "navbar" e o substituindo no id="navbar"
+    } catch (error) { // Caso ocorra algum erro no carregamento, exibe uma mensagem no console
+        console.error('Erro ao carregar a barra de navegação:', error);
+    }
+}
+
+loadNavbar(); // Chama a função para carregar o arquivo
