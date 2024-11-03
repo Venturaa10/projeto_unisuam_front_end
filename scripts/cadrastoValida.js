c931da6c6e7116ccee38effd912b270bdfc2703b
Joao Victor Ventura <joao.victor.ventura25@gmail.com>
2024-11-03 12:06:19 -0300

    Refatorando o arquivo responsavel pelas validações dos campos do formulario de cadastro e adicionando a validação do CPF.

--- scripts/cadrastoValida.js
+++ scripts/cadrastoValida.js
@@ -1,95 +1,79 @@
-const formulario = document.getElementById('idformulario');
+const formulario = document.querySelector('.idformulario');
 const campos = document.querySelectorAll('.input_validate');
 const spans = document.querySelectorAll('.span_mensagem');
 
+console.log(campos)
 
 formulario.addEventListener('submit', event => {
-    let eValido = true;
+    event.preventDefault();
+    const validadores = [
+        validaNome,
+        validaNomeMaterno,
+        validaCPF
+    ];
+    const eValido = validadores.every(validador => validador());
 
-    if (!validaNome()) {
-        eValido = false;
+    if (eValido) {
+        window.location = './index.html';
     }
-    if (!validaCpf()) {
-        eValido = false;
-    }
-    if (!validaLogin()) {
-        eValido = false;
-    }
-    if (!validaSenha()) {
-        eValido = false;
-    }
-    if (!validaConfirmaSenha()) {
-        eValido = false;
-    }
-    if (!eValido) {
-        event.preventDefault();
-    } else {
-        event.preventDefault();
-        localStorage.setItem('loginUsuario', loginUsuario.value);
-        localStorage.setItem('senhaUsuario', senhaUsuario.value);
-        window.location = '/templates/index.html';
-    }
-})
+});
 
 
-function setError(index) {
+function setError(element, span) {
     // Função responsavel por aplicar estilo ao campo e o span do campo se o informação for invalida.
-    campos[index].style.border = '2px solid #e6336e';
-    spans[index].style.display = 'block';
+    element.style.border = '2px solid #e6336e';
+    span.style.display = 'block';
 }
 
-function removeError(index){
+function removeError(element, span){
     // Função responsavel por remover o estilo ao campo e o span quando a informação for valida.
-    campos[index].style.border = '';
-    spans[index].style.display = 'none';
+    element.style.border = '';
+    span.style.display = 'none';
+}
+
+function validaCampo(index, condicao) {
+    if (condicao) {
+        removeError(campos[index], spans[index]);
+        return true;
+
+    } else {
+        setError(campos[index], spans[index]);
+        return false;
+    }
 }
 
 function validaNome() {
     // Função responsavel por validar o campo nome.
-    if(campos[0].value.length < 10)
-    {
-        setError(0);
-        return false;
-    }
-    else {
-        removeError(0);
-        return true;
-    }
+    return validaCampo(0, campos[0].value.length >= 15);
 }
 
-function validaLogin() {
-    // Função responsavel por validar o campo login.
-    if (campos[1].value.length !== 5) {
-        setError(1);
-        return false;
-
-    } else {
-        removeError(1);
-        return true;
-    }
+function validaNomeMaterno() {
+    return validaCampo(1, campos[1].value.length >= 10);
 }
 
-function validaSenha() {
-    // Função responsavel por validar o campo da senha.
-    if (campos[2].value.length !== 8) {
-        setError(2);
-        return false;
-
-    }
-    else {
-        removeError(2);
-        return true;
-    }
+function validaCPF() {
+    return validaCampo(2, TestaCPF(campos[2].value));
 }
 
-function validaConfirmaSenha() {
-    // Função responsavel por validar o campo da confimação de senha.
-    if (campos[2].value === campos[3].value && campos[2].value.length === 8) {
-        removeError(3);
-        return true;
-    } else {
-        setError(3);
-        return false;
+function TestaCPF(strCPF) {
+    var Soma;
+    var Resto;
+    Soma = 0;
+  if (strCPF == "00000000000") return false;
 
-    }
+  for (i=1; i<=9; i++) Soma = Soma + parseInt(strCPF.substring(i-1, i)) * (11 - i);
+  Resto = (Soma * 10) % 11;
+
+    if ((Resto == 10) || (Resto == 11))  Resto = 0;
+    if (Resto != parseInt(strCPF.substring(9, 10)) ) return false;
+
+  Soma = 0;
+    for (i = 1; i <= 10; i++) Soma = Soma + parseInt(strCPF.substring(i-1, i)) * (12 - i);
+    Resto = (Soma * 10) % 11;
+
+    if ((Resto == 10) || (Resto == 11))  Resto = 0;
+    if (Resto != parseInt(strCPF.substring(10, 11) ) ) return false;
+    return true;
 }
+// var strCPF = "12345678909";
+// alert(TestaCPF(strCPF));
