4812bdb7203902d2e1ff607f018243e59636dd75
Joao Victor Ventura <joao.victor.ventura25@gmail.com>
2024-11-20 19:05:30 -0300

    Estilizar conteudo em footer, apagar arquivos em desuso.

--- scripts/cep.js
+++ scripts/cep.js
@@ -1,28 +1,28 @@
-export function preencherEndereco(cep) {
-    const cepFormatado = cep.replace(/\D/g, '');
+// export function preencherEndereco(cep) {
+//     const cepFormatado = cep.replace(/\D/g, '');
 
-    if (cepFormatado.length === 8) {
-        const apiUrl = `https://viacep.com.br/ws/${cepFormatado}/json/`;
+//     if (cepFormatado.length === 8) {
+//         const apiUrl = `https://viacep.com.br/ws/${cepFormatado}/json/`;
 
-        return fetch(apiUrl)
-            .then(response => response.json())
-            .then(data => {
-                if (!data.erro) {
-                    document.getElementById('endereco').value = data.logradouro;
-                    document.getElementById('bairro').value = data.bairro;
-                    document.getElementById('cidade').value = data.localidade;
-                    document.getElementById('estado').value = data.uf;
+//         return fetch(apiUrl)
+//             .then(response => response.json())
+//             .then(data => {
+//                 if (!data.erro) {
+//                     document.getElementById('endereco').value = data.logradouro;
+//                     document.getElementById('bairro').value = data.bairro;
+//                     document.getElementById('cidade').value = data.localidade;
+//                     document.getElementById('estado').value = data.uf;
 
-                    return true; // CEP válido
-                } else {
-                    return false; // CEP inválido
-                }
-            })
-            .catch(error => {
-                console.error('Erro ao buscar o CEP:', error);
-                return false; 
-            });
-    } else {
-        return Promise.resolve(false);
-    }
-}
+//                     return true; // CEP válido
+//                 } else {
+//                     return false; // CEP inválido
+//                 }
+//             })
+//             .catch(error => {
+//                 console.error('Erro ao buscar o CEP:', error);
+//                 return false; 
+//             });
+//     } else {
+//         return Promise.resolve(false);
+//     }
+// }
