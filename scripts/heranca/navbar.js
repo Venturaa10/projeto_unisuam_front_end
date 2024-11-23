c2fb4175043da9496be8d26478d2c07272eeed76
Joao Victor Ventura <joao.victor.ventura25@gmail.com>
2024-11-23 16:50:54 -0300

    Adicionar comentarios as funções.

--- scripts/heranca/navbar.js
+++ scripts/heranca/navbar.js
@@ -1,15 +1,26 @@
 async function loadNavbar() {
+    /** Realizar a herança e as funções que exibem o nome de usuario e botão.
+     * const:
+     * response: Recebe o caminho da navbar.
+     * navbar: Transforma o valor de response em texto.
+     * 
+     * Try:
+     * Buscar o id onde o conteudo da navbar será exibido no HTML.
+     * Substituir o conteúdo HTML pelo valor da variavel "navbar".
+     * Executar novamente as funções showUserName() e setupLogoutButton(), garantindo que o nome de usuario e botão sejam localizados.
+     * 
+     * Em caso de falha, exibe mensagem de error.
+     */
     try {
         const response = await fetch('base/navbar.html');
         const navbar = await response.text();
         document.getElementById('navbar').innerHTML = navbar;
         
-        // Agora, chama showUserName após carregar o navbar.
         showUserName();  
-        setupLogoutButton();  // Configura o botão de logout após o navbar ser carregado, para garantir que encontre o botão.
+        setupLogoutButton();  
     } catch (error) {
         console.error('Erro ao carregar a barra de navegação:', error);
     }
 }
 
-loadNavbar();  // Carrega o navbar e chama showUserName.
+loadNavbar();
--- scripts/validacao/userLocalStorage.js
+++ scripts/validacao/userLocalStorage.js
@@ -1,7 +1,18 @@
-// Função para configurar o botão de logout
 function setupLogoutButton() {
+    /** Configurar botão de logout do site.
+     * const:
+     * logoutButton - Pega o id do botão.
+     * 
+     * Condição:
+     *  logoutButton True:
+     * Botão existe, quando acontecer um clique no botão, "isLoggedIn" é removido do localStorage.
+     * Usuario é redirecionado para a pagina de login.
+     * 
+     * False:
+     * Exibe mensagem informando que botão não foi encontrado.
+     */
     const logoutButton = document.getElementById('logoutButton');
-    console.log('Botão de logout encontrado:', logoutButton);
+    console.log('Botão de logout encontrado:', logoutButton); // Informar mensagem sobre a existencia do botão.
 
     if (logoutButton) {
         logoutButton.addEventListener('click', function () {
@@ -13,8 +24,22 @@ function setupLogoutButton() {
     }
 }
 
-// Função para mostrar o nome do usuário se ele estiver logado
+
 function showUserName() {
+    /** Exibir o nome do usuario e botão para logout quando estiver logado.
+     * const:
+     * userNameElement e logoutButton: Pega id dos elementos no HTML (nesse caso navbar.html).
+     * isLoggedIn: Pega valor do item "isLoggedIn" no localStorage.
+     * 
+     * Condição:
+     * isLoggedIn True:
+     * Usuario está logado.
+     * loginSalvo: Pega o valor da chave "login" no local Storage.
+     * 
+     * Condição if (True e True):
+     * Verificar se existe ambos valores da condição.
+     * Exibir mensagem e aplcar estilos para nome de usuario e botão ficarem visiveis.
+     */
     const userNameElement = document.getElementById('userName');
     const logoutButton = document.getElementById('logoutButton');
     const isLoggedIn = localStorage.getItem('isLoggedIn');
@@ -22,7 +47,7 @@ function showUserName() {
     if (isLoggedIn === 'true') {
         const loginSalvo = localStorage.getItem('login');
         if (userNameElement && logoutButton) {
-            console.log('Botão de logout encontrado na função showUserName:', logoutButton);
+            console.log('Botão de logout encontrado na função showUserName:', logoutButton); // Informar que encontrou a função encontrou o botão.
             userNameElement.style.display = 'inline';  
             userNameElement.textContent = loginSalvo;  
             logoutButton.style.display = 'inline';  
@@ -33,6 +58,6 @@ function showUserName() {
 // Espera o DOM carregar completamente antes de executar
 document.addEventListener('DOMContentLoaded', function () {
     console.log('DOM completamente carregado');
-    setupLogoutButton();  // Configura o botão de logout
-    showUserName();       // Mostra o nome do usuário, se logado
+    setupLogoutButton();  
+    showUserName();   
 });
